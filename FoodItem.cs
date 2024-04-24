5902c7e73b33197dcdfb5309c64a1be92f0929ef
JakubPawlowski99 <56391766+JakubPawlowski99@users.noreply.github.com>
2024-04-24 13:33:27 +0200

    Add files via upload

---
+++ FoodItem.cs
@@ -0,0 +1,8 @@
+﻿public class FoodItem
+{
+    public string Name { get; set; }
+    public double Calories { get; set; }
+    public double Protein { get; set; }
+    public double Fats { get; set; }
+    public double Carbs { get; set; }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MacroCounter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            currentDateLabel.Content = DateTime.Today.ToString("dddd, MMMM d, yyyy");
+        }
+
+        public void LoadFoodItems(ComboBox comboBox)
+        {
+            // Load food items from XML file
+            List<FoodItem> foodItems = FoodItemLoader.LoadFoodItems("foods.xml");
+
+            // Bind food items to the ComboBox
+            comboBox.ItemsSource = foodItems;
+            comboBox.DisplayMemberPath = "Name";
+        }
+    }
+}
